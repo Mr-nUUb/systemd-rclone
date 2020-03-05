27bf1a621b383a4b4f25e5b3719fbe2a896d6356
Josef Schabasser <josef.schabasser@salamantex.com>
2020-03-05 09:37:08 +0100

    add some information about systemd unit destination

--- settings.sh
+++ settings.sh
@@ -8,4 +8,8 @@ __rclone_config="${HOME}/.config/rclone/rclone.conf"
 __regex="^\\[([^,]+)\\]$"
 __systemd_unit_name="rclone-mount"
 __systemd_unit_type="service"
+# Which one is the better place?
+# If we install to the first one and the user edits it, 
+# it gets saved to the second one and our uninstall routine does not remove it...
 __systemd_unit_dest="${HOME}/.local/share/systemd/user"
+#__systemd_unit_dest="${HOME}/.config/systemd/user/"
